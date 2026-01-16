47c446e19a80c3287373e29915b14d8dc1461dd1
HUMUM <humum.shaikh@outlook.com>
2026-01-16 18:49:46 +0530

    Refactor display_info method and call

--- Python_Basics/basic_class_example.py
+++ Python_Basics/basic_class_example.py
@@ -5,7 +5,7 @@ class Student:                                    #class named Student
         self.age = age
         self.id = id
 
-    def display_info(self):
+    def display_info(self):                            #have to pass self as an arguement in methods
         print("Name : " , self.name)
         print("Age : " , self.age)
         print("ID : " , self.id)
@@ -16,10 +16,10 @@ class Student:                                    #class named Student
         else:
             return False
 
+                                    
 
+s1 =  Student("Humum" , 17 , 11)                        #object of class student as s1    
 
-s1 =  Student("Humum" , 17 , 11)
-
-s1.display_info()
+s1.display_info()                                        #calling class method display_info()
 
 print("Voting Eligibility : " , s1.is_eligible_to_vote())
